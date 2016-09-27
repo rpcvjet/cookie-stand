6f388e53633e8bb4bfebe9fd25d34376a051eb2d
Kenneth Ashley Edwards <kenneth.ashley@gmail.com>
2016-09-26 17:28:06 -0700

    calculated avg customer and avg cookies

---
+++ app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+
+var firstandPike = {
+  minCustPerHour: 23,
+  maxCustPerHour: 65,
+  avgCookiesperCust: 6.3,
+  randCustomersPerHour: [],
+  avgCookiesPerHour: [],
+
+
+  calcRandCustomerPerHour: function () {
+    for (var i = 0; i < hours.length; i++) {
+      this.randCustomersPerHour.push(Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1)) + this.minCustPerHour);
+
+    }
+  },
+  calcAvgCookiesPerHour: function () {
+    for (var i = 0; i < hours.length; i++) {
+      this.avgCookiesPerHour[i] = Math.round(this.randCustomersPerHour[i] * this.avgCookiesperCust);
+
+    }
+  },
+};
+
+firstandPike.calcRandCustomerPerHour();
+console.log(firstandPike.randCustomersPerHour);
+firstandPike.calcAvgCookiesPerHour();
+console.log(firstandPike.avgCookiesPerHour);
